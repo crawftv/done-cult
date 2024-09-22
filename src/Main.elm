6fdcce0d888be8d8fec5f75ecf39b91be0508c32
Crawford Collins <github@crawfordc.com>
2024-09-21 23:14:35 -0500

    merge

--- src/Main.elm
+++ src/Main.elm
@@ -39,6 +39,7 @@ type Category
     | Destroyed
 
 
+
 categories : List Category
 categories =
     [ NotKnowing, Action, Done, Destroyed ]
@@ -73,7 +74,6 @@ type Msg
     | MoveTask Int Category
     | Tick Time.Posix
 
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -82,15 +82,8 @@ update msg model =
                 ( model, Cmd.none )
 
             else
-                let
-                    movedTasks =
-                        Task model.nextId model.newTaskContent NotKnowing (getDestroyedAt model.currentTime) :: model.tasks
-
-                    updatedTaskList =
-                        sortTasks movedTasks
-                in
                 ( { model
-                    | tasks = updatedTaskList
+                    | tasks = (Task model.nextId model.newTaskContent NotKnowing (getDestroyedAt model.currentTime)) :: model.tasks
                     , nextId = model.nextId + 1
                     , newTaskContent = ""
                   }
@@ -105,15 +98,18 @@ update msg model =
         MoveTask id category ->
             let
                 movedTasks : List Task
-                movedTasks =
-                    List.map (moveTaskIfMatch id category) model.tasks
-
-                updatedTaskList =
-                    sortTasks movedTasks
+                movedTasks = List.map (moveTaskIfMatch id category) model.tasks
+                notKnowingOrActionList : List Task
+                notKnowingOrActionList =  List.filter (\t -> (t.category == NotKnowing || t.category==Action)) movedTasks
+                doneList: List Task
+                doneList = List.filter (\t -> t.category == Done) movedTasks
+                destroyedList: List Task
+                destroyedList = List.filter (\t -> t.category == Destroyed) movedTasks
+                updatedTaskList = notKnowingOrActionList ++ (doneList ++ destroyedList)
             in
-            ( { model | tasks = List.map (moveTaskIfMatch id category) updatedTaskList }
-            , Cmd.none
-            )
+                ( { model | tasks = List.map (moveTaskIfMatch id category) updatedTaskList }
+                , Cmd.none
+                )
 
         Tick newTime ->
             ( { model
@@ -122,28 +118,6 @@ update msg model =
               }
             , Cmd.none
             )
-
-
-sortTasks : List Task -> List Task
-sortTasks tasks =
-    let
-        actionList : List Task
-        actionList =
-            List.filter (\t -> t.category == Action) tasks
-
-        notKnowingList : List Task
-        notKnowingList =
-            List.filter (\t -> t.category == NotKnowing) tasks
-
-        doneList : List Task
-        doneList =
-            List.filter (\t -> t.category == Done) tasks
-
-        destroyedList : List Task
-        destroyedList =
-            List.filter (\t -> t.category == Destroyed) tasks
-    in
-    actionList ++ notKnowingList ++ doneList ++ destroyedList
 
 
 moveTaskIfMatch : Int -> Category -> Task -> Task
@@ -186,78 +160,49 @@ view model =
         , viewTaskTable model
         ]
 
-
 viewTaskInput : Model -> Html Msg
-viewTaskInput model =
-    div []
-        [ input [ type_ "text", placeholder "New task", value model.newTaskContent, onInput UpdateNewTaskContent ] []
+viewTaskInput model = div [] [ input [ type_ "text", placeholder "New task", value model.newTaskContent, onInput UpdateNewTaskContent ] []
         , button [ onClick AddTask ] [ text "Add Task" ]
         ]
 
 
 viewTask : Time.Posix -> Category -> Task -> Html Msg
 viewTask currentTime currentCategory task =
-    div [ style "border" "1px solid black" ]
-        [ h1 [] [ text task.content ]
+    li [ style "border" "1px solid black" ]
+        [ text task.content
         , div [] (viewTaskButtons currentCategory task)
-        , div [] [ text <| computeTimeLeft currentTime task.destroyedAt ]
+        , div [] [text <| computeTimeLeft currentTime task.destroyedAt]
         ]
 
-
-millisecondsInDay =
-    24 * 60 * 60 * 1000
-
-
-millisecondsInHour =
-    60 * 60 * 1000
-
-
-millisecondsInMinute =
-    60 * 1000
-
-
-computeTimeLeft : Time.Posix -> Time.Posix -> String
+millisecondsInDay = (24 * 60 * 60 * 1000)
+millisecondsInHour = (60 * 60 * 1000)
+millisecondsInMinute = (60*1000)
+computeTimeLeft :  Time.Posix -> Time.Posix -> String
 computeTimeLeft currentTick destroyTime =
-    let
-        destroyTimeMillis =
-            Time.posixToMillis destroyTime
+        let
+            destroyTimeMillis = Time.posixToMillis destroyTime
+            currentTimeMillis = Time.posixToMillis  currentTick
+            timeLeft = destroyTimeMillis - currentTimeMillis
+            days = timeLeft  // millisecondsInDay
+            hoursLeft = timeLeft -  (days * millisecondsInDay)
+            hours = hoursLeft // millisecondsInHour
+            minutesLeft = hoursLeft - (hours*millisecondsInHour)
+            minutes = minutesLeft // millisecondsInMinute
+            secondsLeft = minutesLeft - (minutes * millisecondsInMinute)
+            seconds = secondsLeft // (1000)
 
-        currentTimeMillis =
-            Time.posixToMillis currentTick
 
-        timeLeft =
-            destroyTimeMillis - currentTimeMillis
-
-        days =
-            timeLeft // millisecondsInDay
-
-        hoursLeft =
-            timeLeft - (days * millisecondsInDay)
-
-        hours =
-            hoursLeft // millisecondsInHour
-
-        minutesLeft =
-            hoursLeft - (hours * millisecondsInHour)
-
-        minutes =
-            minutesLeft // millisecondsInMinute
-
-        secondsLeft =
-            minutesLeft - (minutes * millisecondsInMinute)
-
-        seconds =
-            secondsLeft // 1000
-    in
-    "Expires in "
-        ++ String.fromInt days
-        ++ ":"
-        ++ String.fromInt hours
-        ++ ":"
-        ++ String.fromInt minutes
-        ++ ":"
-        ++ String.fromInt seconds
-
+        in
+            (
+            "Expires in "
+            ++ String.fromInt days
+            ++ ":"
+            ++ String.fromInt hours
+            ++ ":"
+            ++ String.fromInt minutes
+             ++ ":"
+            ++ String.fromInt seconds
+            )
 
 viewTaskButtons : Category -> Task -> List (Html Msg)
 viewTaskButtons currentCategory task =
@@ -304,56 +249,43 @@ viewTH category =
     th [] [ text (categoryToString category) ]
 
 
-viewTaskTable : Model -> Html Msg
+viewTaskTable :Model -> Html Msg
 viewTaskTable model =
     table [ style "width" "100%", style "border-collapse" "collapse" ]
         [ thead []
             [ tr [] <| List.map viewTH categories ]
-        , tbody [] (List.map (viewTaskRow model.currentTime) model.tasks)
-        ]
+            , tbody [] (List.map (viewTaskRow model.currentTime) model.tasks)
+            ]
 
 
-viewTaskRow : Time.Posix -> Task -> Html Msg
+viewTaskRow :Time.Posix -> Task -> Html Msg
 viewTaskRow currentTime task =
+
     tr []
-        [ viewCategoryCell currentTime NotKnowing task
+        [
+          viewCategoryCell currentTime NotKnowing task
         , viewCategoryCell currentTime Action task
         , viewCategoryCell currentTime Done task
         , viewCategoryCell currentTime Destroyed task
         ]
 
-
-viewCategoryCell : Time.Posix -> Category -> Task -> Html Msg
+viewCategoryCell : Time.Posix-> Category -> Task -> Html Msg
 viewCategoryCell currentTime category task =
     td
-        [ style "text-align" "center"
+         [ style "text-align" "center"
         , style "background-color"
             (if task.category == category then
-                case category of
-                    NotKnowing ->
-                        "#e0e0e0"
-
-                    Action ->
-                        "#B1FC8EFF"
-
-                    Done ->
-                        "#eae86b"
-
-                    Destroyed ->
-                        "#7B9FE1FF"
+                "#e0e0e0"
 
              else
                 "white"
             )
         ]
-        [ if task.category == category then
+        [
+            if (task.category == category) then
             viewTask currentTime category task
-
           else
             text ""
         ]
-
-
 getDestroyedAt : Time.Posix -> Time.Posix
-getDestroyedAt currentTime =
-    Time.millisToPosix <| Time.posixToMillis currentTime + (14 * millisecondsInDay)
+getDestroyedAt currentTime = Time.millisToPosix <| (Time.posixToMillis currentTime) + (14 * millisecondsInDay)
