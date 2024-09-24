06cad14aea331a37ba3a01465dc8b53ec9b2813c
Crawford Collins <github@crawfordc.com>
2024-09-24 11:11:11 -0500

    fixed variables

--- done.sh
+++ done.sh
@@ -3,4 +3,4 @@ docker service rm done; \
 docker service create \
   --name done \
   --mount type=bind,source=$HOME/.cloudflared/,target=/app/cloudflared,readonly \
-  637423200832.dkr.ecr.us-east-1.amazonaws.com/crawfordc:done
+  $AWS_REGISTRY_LOCATION/crawfordc:done
